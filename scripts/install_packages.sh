ac2f10f6b148d5df06950c0d46338247b6ae86c4
Pedro Henrique Souza Meinen <pedromeinen99@gmail.com>
2025-11-14 21:39:47 -0300

    feat: Adiciona o script que instala os pacotes

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -2,6 +2,9 @@
 
 source ./config/packages.conf
 
-for item in "${PACKAGES[@]}"; do
-  echo "Package: $item"
-done
+if ! command -v yay >/dev/null 2>&1; then
+  echo "comando yay nao foi encontrado. Instale o programa e tente novamente"
+  exit 1
+fi
+
+yay -S --needed --noconfirm --answerdiff=None --answeredit=None "${PACKAGES[@]}"
