7970a5bb5127d3c98e9b02f1ca007c0c1ffc2700
Pedro Henrique Souza Meinen <pedromeinen99@gmail.com>
2025-11-20 13:20:08 -0300

    feat: remove o submodule e altera para clonar o repositorio de forma direta

--- scripts/setup_yazi.sh
+++ scripts/setup_yazi.sh
@@ -1,17 +1,1 @@
-#!/usr/bin/env bash
 
-if ! command -v pacman >/dev/null 2>&1; then
-  echo "comando pacman, nao foi encontrado. Instale o programa e tente novamente"
-  exit 1
-fi
-
-if ! command -v git >/dev/null 2>&1; then
-  echo "comando git, nao foi encontrado. Instale o programa e tente novamente"
-  exit 1
-fi
-
-sudo pacman -S --needed base-devel
-
-git clone https://aur.archlinux.org/yay.git ~/yay
-cd ~/yay
-makepkg -si
